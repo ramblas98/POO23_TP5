bfac2e2f8263a52be679ebd392269989213dc645
Sans <Sans@Sans-PC>
2023-05-31 17:33:34 -0300

    Primera Version

---
+++ TP05_EJE01/src/tp05_eje01/TP05_EJE01.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp05_eje01;
+
+/**
+ *
+ * @author Sans
+ */
+public class TP05_EJE01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
