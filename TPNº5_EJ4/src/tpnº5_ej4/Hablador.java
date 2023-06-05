ed93183d4faa5fde969af241d9ba36b264b27861
Cossio Carina <Cossio Carina@LAPTOP-ODGO697K>
2023-06-05 16:48:36 -0300

    InicioEj4

---
+++ TPNº5_EJ4/src/tpnº5_ej4/Hablador.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package tpnº5_ej4;
+
+
+public interface Hablador {
+    
+    public void saluda();
+}
