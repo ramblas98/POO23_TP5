83b9079eb0ec314be5bca62d01bf19e73da8990f
Cossio Carina <Cossio Carina@LAPTOP-ODGO697K>
2023-06-05 16:35:12 -0300

    FinalEj3

---
+++ TPNº5_EJ3/src/tpnº5_ej3/TPNº5_EJ3.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tpnº5_ej3;
+
+
+public class TPNº5_EJ3 {
+
+    
+    public static void main(String[] args) {
+        
+        Animal a1 = new Perro("Rocky", 2);
+        Animal a2 = new Gato("Nieve", 1);
+        
+        Persona p1 = new Programador("Jonatan", 21);
+        
+        a1.saluda();
+        a2.saluda();
+        p1.saluda();
+        
+    }
+    
+}
